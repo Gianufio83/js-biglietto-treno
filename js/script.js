879d3f013754378018e9fb16963c16916d97bb48
Gianufio83 <fiore.gianuario@gmail.com>
2019-12-18 15:33:39 +0100

    prompt con km da percorrere ed età dell'utente

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// alert("Ciao");
+// Il programma dovrà chiedere all’utente il numero di chilometri che vuole
+// percorrere e l’età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
+// Il prezzo del biglietto è definito in base ai km (0.21 € al km), ma va applicato uno sconto del 20% per i minorenni e del 40% per gli over 65.
+
+// chiedere all'utente il numero di km che vuole percorrere
+var kilometri = parseInt(prompt("Quanti km hai intenzione di percorrere?"));
+console.log(kilometri);
+// chiedere all'utene la sua età
+var anni = parseInt(prompt("Indica la tua età"));
+console.log(anni);
