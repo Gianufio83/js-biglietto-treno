2e1f499cce78f4e356f576d4d317c9c3a9dc784c
Gianufio83 <fiore.gianuario@gmail.com>
2019-12-18 17:06:37 +0100

    completamento prezzi per fasce d'età

--- js/script.js
+++ js/script.js
@@ -10,3 +10,35 @@ console.log(kilometri);
 // chiedere all'utene la sua età
 var anni = parseInt(prompt("Indica la tua età"));
 console.log(anni);
+// tariffa 18-65 anni
+var tariffaStandard = kilometri * 0.21;
+console.log(tariffaStandard);
+// tariffa 0-18 anni
+var under18 = ((tariffaStandard - 20) / 100) ;
+console.log(under18);
+// // tariffa dopo i 65 anni
+var over65 = ((tariffaStandard - 40) / 100) ;
+console.log(over65);
+
+
+// prezzi suddivisi per fasce d'età
+if ((anni >= 18) && (anni <= 65)) {
+  console.log(tariffaStandard);
+}
+else if (anni <= 18) {
+  console.log(under18);
+} else {
+    console.log(over65);
+  }
+
+// else (anni >= 65) {
+//   .console.log(over65);
+// }
+// if (anni <= 18) {
+//   console.log(under18);
+// } else if (anni >= 18) && (anni <= 65) {
+//   console.log(tariffaStandard);
+// } else (anni > 65) {
+//   console.log(over65);
+// }
+// }
